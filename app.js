4c7ec987455bec012165d26ae1b0753e303e6d73
Saimun <saimun4u@gmail.com>
2021-06-23 20:20:30 -0500

    JS logic set to show results using fetch API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const search = document.getElementById('search'),
     submit = document.getElementById('submit'),
     random = document.getElementById('random'),
-    meals = document.getElementById('meals'),
+    mealsEl = document.getElementById('meals'),
     resultHeading = document.getElementById('result-heading'),
     single_meal = document.getElementById('single-meals')
 
@@ -24,8 +24,26 @@ function searchMeal(e){
 
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data.meals)
+            resultHeading.innerHTML = `<h2>Showing search results for '${item}': </h2>`
 
+            if (data.meals === null){
+                resultHeading.innerHTML = `<p>There are no search results. Please try again</p>`
+            }else{
+                mealsEl.innerHTML = data.meals.map(meal => 
+                    `<div class = "meal">
+                        <img src="${meal.strMealThumb}" alt = "${meal.strMeal}"/>
+                        <div class="meal-info" data-mealID = "${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div> 
+                    </div>
+                    `)
+                    .join('')
+            }
+        });
+        //Clear search value
+        search.value = '';
     }else{
         alert('Please enter name of a meal to be searched');
     }
@@ -33,7 +51,6 @@ function searchMeal(e){
     e.preventDefault();
     
 }
-
 
 //Set event listeners
 
