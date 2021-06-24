f7333720c36b71bde4f1e6d3efac789f845aaee3
Saimun <saimun4u@gmail.com>
2021-06-23 19:42:32 -0500

    Use fetch API to fetch data on food items

--- app.js
+++ app.js
@@ -10,9 +10,28 @@ const search = document.getElementById('search'),
 
 function searchMeal(e){
 
-    console.log(search.value);
+    //Clear single meal value
+
+    single_meal.innerHTML = '';
+
+    //Get value of item entered in search box
+
+    const item = search.value;
+
+    //Check for empty search item
+
+    if(item.trim()){
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    }else{
+        alert('Please enter name of a meal to be searched');
+    }
 
     e.preventDefault();
+    
 }
 
 
